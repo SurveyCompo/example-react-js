c31e6aa54ed9d4c30b98357a99208a9218d9a15b
David Lin <>
2024-07-05 15:05:57 +1000

    update layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,63 +1,68 @@
-import { useEffect, useRef, useState } from 'react'
-import './App.css'
-import localExample from './example.json'
+import { useEffect, useRef, useState } from 'react';
+import './App.css';
+import localExample from './example.json';
 
 function App() {
   const surveys: Record<string, unknown> = {
     survey1: 'https://raw.githubusercontent.com/SurveyCompo/examples/main/examples/inputs/source.json',
     survey2: 'https://raw.githubusercontent.com/SurveyCompo/examples/main/examples/style/source.json',
-    local: JSON.stringify(localExample)
-  }
+    local: JSON.stringify(localExample),
+  };
 
   const [sourceName, setSourceName] = useState('survey1');
 
   const surveySource = surveys[sourceName];
-  const surveyElement = useRef<{src: unknown , api: {reset: ()=>void}}>();
+  const surveyElement = useRef<{ src: unknown; api: { reset: () => void; goNext: () => void; goPrev: () => void } }>();
 
   const onSurveyChange = (event: Event) => {
     const customEventDetail = (event as CustomEvent).detail;
     console.log(customEventDetail);
-  }
+  };
 
   const resetSurvey = () => {
     surveyElement.current?.api.reset();
-  }
+  };
+  const goNext = () => {
+    surveyElement.current?.api.goNext();
+  };
+  const goPrev = () => {
+    surveyElement.current?.api.goPrev();
+  };
 
   const setDataToInstance = () => {
     if (surveyElement.current) {
       surveyElement.current.src = localExample;
     }
-  }
+  };
 
   useEffect(() => {
-    window.addEventListener("surveychange", onSurveyChange);
+    window.addEventListener('surveychange', onSurveyChange);
     return () => {
-      window.removeEventListener("surveychange", onSurveyChange);
+      window.removeEventListener('surveychange', onSurveyChange);
     };
-  }, [sourceName, onSurveyChange])
+  }, [sourceName, onSurveyChange]);
 
   return (
     <>
-      <h1 className='title'>React.js Integration Example</h1>
-      <div className='survey-container'>
-        <survey-compo
-          ref={surveyElement}
-          cache="memory"
-          src={surveySource} />
-      </div>
-
-      <div className='button-group'>
-        <button onClick={() => setSourceName('survey1')}>Survey 1</button>
-        <button onClick={() => setSourceName('survey2')}>Survey 2</button>
-        <button onClick={() => setSourceName('local')}>Local</button>
-      </div>
-
-      <div className='button-group'>
-        <button onClick={() => resetSurvey()}>Reset</button>
-        <button onClick={() => setDataToInstance()}>Set JSON Data</button>
+      <h1 className="title">React.js Integration Example</h1>
+      <div className="layout">
+        <div className="button-group">
+          <h3>Load Survey</h3>
+          <button onClick={() => setSourceName('survey1')}>Remote JSON 1</button>
+          <button onClick={() => setSourceName('survey2')}>Remote JSON 2</button>
+          <button onClick={() => setSourceName('local')}>Local JSON attr</button>
+          <button onClick={() => setDataToInstance()}>Local JSON prop</button>
+          <h3>API interactions</h3>
+          <button onClick={() => resetSurvey()}>Reset</button>
+          <button onClick={() => goPrev()}> &#8592; Prev</button>
+          <button onClick={() => goNext()}> &#8594; Next</button>
+        </div>
+        <div className="survey-container">
+          <survey-compo ref={surveyElement} cache="memory" src={surveySource} />
+        </div>
       </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
